461d08651a6c82d49c7008f1cd6f9f3fb6f23589
Udit Arora <71641952+ud1tt@users.noreply.github.com>
2024-02-10 06:10:37 +0530

    Time: 13 ms (94.34%), Space: 20.4 MB (23.37%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.cpp
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -1,62 +1,87 @@
 class MyCircularQueue {
-private:
-    int *arr;
-    int front;
-    int rear;
-    int size;
-
+    private:
+        int *arr;
+        int front;
+        int rear;
+        int size;
 public:
     MyCircularQueue(int k) {
-        size = k + 1; // Increase size by 1 to distinguish between empty and full condition
+        size = k;
         arr = new int[size];
-        front = 0;
-        rear = 0;
+        front = -1;
+        rear = -1;
     }
-
+    
     bool enQueue(int value) {
-        if (isFull()) {
+        if(isFull()){
             return false;
         }
-        else {
-            rear = (rear + 1) % size;
-            arr[rear] = value;
-            return true;
+        else if(front==-1){
+            front = rear = 0;
         }
+        else if(rear==size-1 && front!=0){
+            rear=0;
+        }
+        else{
+            rear++;
+        }
+        arr[rear]=value;
+        return true;
     }
-
+    
     bool deQueue() {
-        if (isEmpty()) {
+        if(isEmpty()){
             return false;
         }
-        else {
-            front = (front + 1) % size;
-            return true;
+        else if(front==rear){
+            front = rear = -1;
         }
+        else if(front==size-1){
+            front = 0;
+        }
+        else{
+            front++;
+        }
+        return true;
     }
-
+    
     int Front() {
-        if (isEmpty()) {
+        if(isEmpty()){
             return -1;
         }
-        else {
-            return arr[(front + 1) % size];
+        else{
+            return arr[front];
         }
     }
-
+    
     int Rear() {
-        if (isEmpty()) {
+        if(isEmpty()){
             return -1;
         }
-        else {
+        else{
             return arr[rear];
         }
     }
-
+    
     bool isEmpty() {
-        return front == rear;
+        return (front==-1);
     }
-
+    
     bool isFull() {
-        return (rear + 1) % size == front;
+        if(front==0 && rear==size-1 || (rear + 1) % size == front)
+            return true;
+        else
+            return false;
     }
 };
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
