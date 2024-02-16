e6c2736b6c65fd9842ef1ce3233c11811d5261d3
Udit Arora <71641952+ud1tt@users.noreply.github.com>
2024-02-16 07:37:42 +0530

    Time: 32 ms (16.01%), Space: 21.6 MB (17.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> nums2;
-        int count=0;
-        for(int i=0;i<nums.size();i++){
+        // vector<int> nums2;
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0){
+        //         count++;
+        //         continue;
+        //     }
+        //     nums2.push_back(nums[i]);
+        // }
+        // nums.clear();
+        // for(int i=0;i<nums2.size();i++){
+        //     nums.push_back(nums2[i]);
+        // }
+        // for(int i=0;i<count;i++){
+        //     nums.push_back(0);
+        // }
+        int count= 0;
+        for (int i=0; i<nums.size();i++){
             if(nums[i]==0){
                 count++;
-                continue;
+                nums.erase(nums.begin() + i);
+                i--;
             }
-            nums2.push_back(nums[i]);
-        }
-        nums.clear();
-        for(int i=0;i<nums2.size();i++){
-            nums.push_back(nums2[i]);
         }
         for(int i=0;i<count;i++){
             nums.push_back(0);
