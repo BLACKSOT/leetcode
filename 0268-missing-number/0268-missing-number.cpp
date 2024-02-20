c2d53b7195d163f2e63a2098d146be7c3c44afdb
Udit Arora <71641952+ud1tt@users.noreply.github.com>
2024-02-20 07:06:48 +0530

    Time: 34 ms (9.22%), Space: 28.1 MB (5.76%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,18 +2,36 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        int n = nums.size();
+//         int n = nums.size();
         
-        for(int i = 0;i<nums.size();i++){
-            for(int j =0;j<nums.size();j++){
-                if(i==nums[j]){
-                    break;
-                }
-                else if(i!=nums[j] && j==nums.size()-1){
-                    return i;
-                }
+//         for(int i = 0;i<nums.size();i++){
+//             for(int j =0;j<nums.size();j++){
+//                 if(i==nums[j]){
+//                     break;
+//                 }
+//                 else if(i!=nums[j] && j==nums.size()-1){
+//                     return i;
+//                 }
+//             }
+//         }
+//         return n;
+//     }
+        
+        set<int> s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        nums.clear();
+        for(int it: s){
+            nums.push_back(it);
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(i!=nums[i]){
+                return i;
             }
         }
-        return n;
+        return nums.size();
     }
 };
+    
