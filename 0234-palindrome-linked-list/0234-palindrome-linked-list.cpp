a70a4f4d16eb0e0ab7ee64ae92879121fb76e08d
Udit Arora <71641952+ud1tt@users.noreply.github.com>
2024-02-23 02:38:55 +0530

    Time: 166 ms (73.15%), Space: 123.5 MB (36.12%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,19 +10,31 @@
  */
 class Solution {
 public:
+    ListNode* tail;
     bool isPalindrome(ListNode* head) {
-        ListNode* tail = head;\
-        vector<int> res;
-        while(tail!=NULL){
-            res.push_back(tail->val);
-            tail = tail->next;
+        // ListNode* tail = head;
+        // vector<int> res;
+        // while(tail!=NULL){
+        //     res.push_back(tail->val);
+        //     tail = tail->next;
+        // }
+        // int n = res.size();
+        // for(int i=0;i<n;i++){
+        //     if(res[i]!=res[--n]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+        tail = head;
+        return check(head);    
+    }
+    bool check(ListNode* p){
+        if(p==NULL){
+            return true;
         }
-        int n = res.size();
-        for(int i=0;i<n;i++){
-            if(res[i]!=res[--n]){
-                return false;
-            }
-        }
-        return true;
+        bool check1 = check(p->next) && (p->val == tail->val);
+        tail = tail->next;
+        return check1;
     }
 };
