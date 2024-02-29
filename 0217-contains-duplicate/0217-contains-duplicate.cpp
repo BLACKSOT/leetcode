773afb6d21b69d4d7999ad601fe8b7e4795085cf
Udit Arora <71641952+ud1tt@users.noreply.github.com>
2024-03-01 04:15:49 +0530

    Time: 84 ms (86.94%), Space: 73.1 MB (45.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> b;
+//         unordered_map<int,int> b;
         
-        for(int i=0;i<nums.size();i++){
-            b[nums[i]]++;
-            if(b[nums[i]]>1){
+//         for(int i=0;i<nums.size();i++){
+//             b[nums[i]]++;
+//             if(b[nums[i]]>1){
+//                 return true;
+//             }
+//         }
+//         return false;
+        
+        unordered_set<int> s;
+        for(int num:nums){
+            if(s.count(num)){
                 return true;
+            }
+            else{
+                s.insert(num);
             }
         }
         return false;
