77439422ad2a4788a26a94d979ae64097d9beb19
Udit Arora <71641952+ud1tt@users.noreply.github.com>
2024-03-02 18:54:32 +0530

    Time: 27 ms (31.97%), Space: 76 MB (56.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -6,20 +6,13 @@ public:
         int carry = 0;
         
         while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int sum = carry;
-            if (l1 != nullptr) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if (l2 != nullptr) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            carry = sum / 10;
-            sum %= 10;
-            
-            current->next = new ListNode(sum);
-            current = current->next;
+        int sum = (l1!=nullptr ? l1->val : 0) + (l2!=nullptr ? l2->val : 0) + carry;
+        carry = sum/10;
+        sum = sum%10;
+        current->next = new ListNode(sum);
+        current = current ->next;
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
         }
         
         ListNode* result = dummy->next;
